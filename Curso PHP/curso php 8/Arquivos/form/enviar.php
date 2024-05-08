7436bc2477d90adaea9236353c66a3854aa78779
Bruno Canto <bruno_scanto@hotmail.com>
2024-05-07 22:13:28 -0300

    aula 20

--- Curso PHP/curso php 8/Arquivos/form/enviar.php
+++ Curso PHP/curso php 8/Arquivos/form/enviar.php
@@ -3,8 +3,15 @@
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $mensagem = $_POST['mensagem'];
+$assunto = 'Email do Site';
+$remetente = 'contato@hugocursos.com.br';
 
-$conteudo = utf8_decode('Nome: '. $nome . "\r\n" . "\r\n" . 'Email: '. $email . "\r\n" . "\r\n" . 'Mensagem: '. $mensagem . "\r\n" . "\r\n");
+$conteudo = 'Nome: ' .$nome ."\r\n"."\r\n" .'Email: ' .$email ."\r\n"."\r\n"."\r\n" .'Mensagem: ' .$mensagem ."\r\n"."\r\n";
 
-$c
-?>
+$cabecalhos = "From: ".$email;
+
+mail($remetente, $assunto, $conteudo, $cabecalhos);
+
+ ?>
+<script>alert('Enviado com Sucesso');</script>
+<meta http-equiv="refresh" content="0; url=index.php">
