15ae081fe392c8b2a535b80432a244369d131861
Bruno Canto <bruno_scanto@hotmail.com>
2024-05-19 22:12:18 -0300

    segurança

--- Curso PHP/curso php 8/Arquivos/login/index.php
+++ Curso PHP/curso php 8/Arquivos/login/index.php
@@ -1,12 +1,18 @@
+<?php
+require_once("../conexao.php");
+?>
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<link rel="stylesheet" href="login.css">
+<link href="login.css" rel="stylesheet">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <!------ Include the above in your HEAD tag ---------->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
 
 <body>
     <div id="login">
-        <h3 class="text-center text-white pt-5">Formulário de login</h3>
+
         <div class="container">
             <div id="login-row" class="row justify-content-center align-items-center">
                 <div id="login-column" class="col-md-6">
@@ -19,13 +25,14 @@
                             </div>
                             <div class="form-group">
                                 <label for="password" class="text-info">Senha:</label><br>
-                                <input type="text" name="senha" id="password" class="form-control" placeholder="Insira sua Senha">
+                                <input type="password" name="senha" id="password" class="form-control" placeholder="Insira sua Senha" required>
                             </div>
                             <div class="form-group">
+
                                 <input type="submit" name="submit" class="btn btn-info btn-md" value="Logar">
                             </div>
                             <div id="register-link" class="text-right mt-1">
-                                <a href="#" class="text-info">Cadastre-se</a>
+                                <a href="" data-toggle="modal" data-target="#modal-cadastrar" class="text-info">Cadastre-se</a>
                             </div>
                         </form>
                     </div>
@@ -34,3 +41,34 @@
         </div>
     </div>
 </body>
+
+
+
+
+<div class="modal" id="modal-cadastrar" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Cadastre-se</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email </label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                <button type="button" class="btn btn-primary">Salvar</button>
+            </div>
+        </div>
+    </div>
+</div>
--- Curso PHP/curso php 8/Arquivos/login/painel-adm/index.php
+++ Curso PHP/curso php 8/Arquivos/login/painel-adm/index.php
@@ -2,6 +2,15 @@
 
 @session_start();
 
+//VERIFICAR SE TEM UM USUÁRIO LOGADO E SE ELE É ADMINISTRADOR
+if(@$_SESSION['nivel_usuario'] != 'Administrador'){
+  echo "<script language='javascript'>window.location='../index.php'</script>";
+}
+ 
+
 echo 'Painel Administrativo <p>';
-echo 'Nome do Usuário: ' . $_SESSION['nome_usuario'] . 'e o nível do usuário é ' .  $_SESSION['nivel_usuario'];
+echo 'Nome do Usuário: ' . $_SESSION['nome_usuario'] . ' e o nível do usuário é ' .  $_SESSION['nivel_usuario'];
+
 ?>
+
+<a href="../logout.php">Sair</a>
