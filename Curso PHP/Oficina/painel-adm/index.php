31a3f236c5a1499dd2dd59e44b7d25934b0874e2
Bruno Canto <bruno_scanto@hotmail.com>
2024-06-30 00:00:50 -0300

    menus

--- Curso PHP/Oficina/painel-adm/index.php
+++ Curso PHP/Oficina/painel-adm/index.php
@@ -2,8 +2,8 @@
 
     //variaveis para o menu
     $pag = @$_GET["pag"];
-    $menu1 = "menu1";
-    $menu2 = "menu2";
+    $menu1 = "mecanico";
+    $menu2 = "recepcionista";
     $menu3 = "menu3";
     $menu4 = "menu4";
     $menu5 = "menu5";
@@ -20,16 +20,16 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="">
-        <meta name="author" content="Hugo Vasconcelos">
+        <meta name="author" content="Bruno Canto">
         
 
         <title>Painel Administrativo</title>
 
-        <!-- Custom fonts for this template-->
+        <!-- Fontes personalizadas para este modelo-->
         <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-        <!-- Custom styles for this template-->
+        <!-- Estilos personalizados para ese modelo-->
         <link href="../css/sb-admin-2.min.css" rel="stylesheet">
         <link href="../css/style.css" rel="stylesheet">
         
@@ -47,27 +47,27 @@
 
     <body id="page-top">
 
-        <!-- Page Wrapper -->
+        <!--Wrapper da página-->
         <div id="wrapper">
 
-            <!-- Sidebar -->
+            <!-- Barra Lateral -->
             <ul class="navbar-nav bg-gradient-danger sidebar sidebar-dark accordion" id="accordionSidebar">
 
-                <!-- Sidebar - Brand -->
+                <!-- Marca da barra lateral -->
                 <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
 
                     <div class="sidebar-brand-text mx-3">Administrador</div>
                 </a>
 
-                <!-- Divider -->
+                <!-- Divisor -->
                 <hr class="sidebar-divider my-0">
 
 
 
-                <!-- Divider -->
+                <!-- Divisor -->
                 <hr class="sidebar-divider">
 
-                <!-- Heading -->
+                <!-- Cabeçalho -->
                 <div class="sidebar-heading">
                     Cadastros
                 </div>
@@ -82,13 +82,13 @@
                     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                         <div class="bg-white py-2 collapse-inner rounded">
                             <h6 class="collapse-header">PESSOAS:</h6>
-                            <a class="collapse-item" href="index.php?pag=<?php echo $menu1 ?>">Menu 1</a>
+                            <a class="collapse-item" href="index.php?pag=<?php echo $menu1 ?>">Mecanicos</a>
                             <a class="collapse-item" href="index.php?pag=<?php echo $menu2 ?>">Menu 2</a>
                         </div>
                     </div>
                 </li>
 
-                <!-- Nav Item - Utilities Collapse Menu -->
+                <!-- Item de navegação - Menu de colapso de utilitários -->
                 <li class="nav-item">
                     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
                         <i class="fas fa-home"></i>
@@ -105,47 +105,47 @@
                     </div>
                 </li>
 
-                <!-- Divider -->
+                <!-- Divisor -->
                 <hr class="sidebar-divider">
 
-                <!-- Heading -->
+                <!-- Cabeçalho -->
                 <div class="sidebar-heading">
                     Consultas
                 </div>
 
 
 
-                <!-- Nav Item - Charts -->
+                <!-- Item de navegação - gráficos -->
                 <li class="nav-item">
                     <a class="nav-link" href="index.php?pag=<?php echo $menu6 ?>">
                         <i class="fas fa-fw fa-chart-area"></i>
                         <span>Menu 6</span></a>
                 </li>
 
-                <!-- Nav Item - Tables -->
+                <!-- Item de Navegação- Tabelas -->
               
 
-                <!-- Divider -->
+                <!-- Divisor -->
                 <hr class="sidebar-divider d-none d-md-block">
 
-                <!-- Sidebar Toggler (Sidebar) -->
+                <!-- Alternador da barra lateral (barra lateral) -->
                 <div class="text-center d-none d-md-inline">
                     <button class="rounded-circle border-0" id="sidebarToggle"></button>
                 </div>
 
             </ul>
-            <!-- End of Sidebar -->
+            <!-- Fim da barra lateral -->
 
-            <!-- Content Wrapper -->
+            <!-- Wrapper de conteúdo -->
             <div id="content-wrapper" class="d-flex flex-column">
 
-                <!-- Main Content -->
+                <!-- Conteúdo principal -->
                 <div id="content">
 
-                    <!-- Topbar -->
+                    <!-- Barra Superior -->
                     <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-                        <!-- Sidebar Toggle (Topbar) -->
+                        <!-- Alternar barra lateral (barra superior) -->
                         <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                             <i class="fa fa-bars"></i>
                         </button>
@@ -153,19 +153,19 @@
 
 
 
-                        <!-- Topbar Navbar -->
+                        <!-- Barra superior intermediária -->
                         <ul class="navbar-nav ml-auto">
 
 
 
-                            <!-- Nav Item - User Information -->
+                            <!-- Item de navegação - Informações do usuário -->
                             <li class="nav-item dropdown no-arrow">
                                 <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     <span class="mr-2 d-none d-lg-inline text-gray-600 small">Nome do usuario</span>
                                     <img class="img-profile rounded-circle" src="../img/sem-foto.jpg">
 
                                 </a>
-                                <!-- Dropdown - User Information -->
+                                <!-- Menu suspenso - Informações do usuário -->
                                 <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
                                     <a class="dropdown-item" href="" data-toggle="modal" data-target="#ModalPerfil">
                                         <i class="fas fa-user fa-sm fa-fw mr-2 text-primary"></i>
@@ -183,9 +183,9 @@
                         </ul>
 
                     </nav>
-                    <!-- End of Topbar -->
+                    <!-- Fim da barra superior -->
 
-                    <!-- Begin Page Content -->
+                    <!-- Conteúdo da página inicial -->
                     <div class="container-fluid">
 
                         <?php if (@$pag == null) { 
@@ -222,17 +222,17 @@
                     <!-- /.container-fluid -->
 
                 </div>
-                <!-- End of Main Content -->
+                <!-- Fim do conteúdo principal -->
 
 
 
             </div>
-            <!-- End of Content Wrapper -->
+            <!-- Fim do wrapper de conteúdo -->
 
         </div>
-        <!-- End of Page Wrapper -->
+        <!-- Wrapper de fim de página -->
 
-        <!-- Scroll to Top Button-->
+        <!-- Role até o botão superior -->
         <a class="scroll-to-top rounded" href="#page-top">
             <i class="fas fa-angle-up"></i>
         </a>
@@ -319,24 +319,24 @@
         </div>
 
 
-        <!-- Core plugin JavaScript-->
+        <!-- Plug-in principal JavaScript-->
         <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
 
-        <!-- Custom scripts for all pages-->
+        <!-- Scripts personalizados para todas as páginas -->
         <script src="../js/sb-admin-2.min.js"></script>
 
-        <!-- Page level plugins -->
+        <!-- Plug-ins de nível de página -->
         <script src="../vendor/chart.js/Chart.min.js"></script>
 
-        <!-- Page level custom scripts -->
+        <!-- Scripts personalizados em nível de página -->
         <script src="../js/demo/chart-area-demo.js"></script>
         <script src="../js/demo/chart-pie-demo.js"></script>
 
-        <!-- Page level plugins -->
+        <!-- Plug-ins de nível de página -->
         <script src="../vendor/datatables/jquery.dataTables.min.js"></script>
         <script src="../vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
-        <!-- Page level custom scripts -->
+        <!-- Scripts personalizados em nível de página -->
         <script src="../js/demo/datatables-demo.js"></script>
 
     </body>
