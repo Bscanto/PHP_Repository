c708541997730ca96336613c9e9100b25abd9c3b
Bruno Canto <bruno_scanto@hotmail.com>
2024-08-26 19:04:10 -0300

    Inserindo nas compras

--- Curso PHP/Oficina/painel-adm/estoque/inserir.php
+++ Curso PHP/Oficina/painel-adm/estoque/inserir.php
@@ -1,5 +1,6 @@
-<?php
-require_once("../../conexao.php");
+<?php 
+require_once("../../conexao.php"); 
+@session_start();
 
 
 $fornecedor = $_POST['fornecedor'];
@@ -8,14 +9,14 @@ $valor_venda = $_POST['valor_venda'];
 $estoque = $_POST['quantidade'];
 $id = $_POST['txtid2'];
 
+$valor_total = $valor_compra * $estoque;
 
 $query = $pdo->query("SELECT * FROM produtos where id = '$id' ");
-$res = $query->fetchAll (PDO::FETCH_ASSOC);
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
 $tot_estoque = $res[0]['estoque'];
-
 $estoque = $tot_estoque + $estoque;
 
-$valor_total = $valor_compra * $estoque
+
 
 if($valor_venda == ""){
 	echo 'O Valor da venda é Obrigatório!';
@@ -33,10 +34,12 @@ if($estoque == ""){
 }
 
 
+$res = $pdo->prepare("UPDATE produtos SET fornecedor = :fornecedor, valor_compra = :valor_compra, valor_venda = :valor_venda, estoque = :estoque WHERE id = '$id'");
 
-	$res = $pdo->prepare("UPDATE produtos SET fornecedor = :fornecedor, valor_compra = :valor_compra, valor_venda = :valor_venda, estoque = :estoque WHERE id = '$id' ");
+$res2 = $pdo->prepare("INSERT INTO contas_pagar SET descricao = 'Compra de Produtos', valor = :valor, funcionario = :funcionario, data = curDate(), data_venc = curDate(), pago = 'Não' ");
 
-	$res2 = $pdo->prepare("INSERT INTO contas_pagar SET descricao = 'Compra de Produtos', valor = :valor, funcionario = :funcionario, data = curData(),data_venc = curDate(), pago = 'Não' ");
+$res3 = $pdo->prepare("INSERT INTO compras SET produto = '$id', valor = :valor, funcionario = :funcionario, data = curDate(), id_conta = :id_conta ");
+
 
 $res->bindValue(":fornecedor", $fornecedor);
 $res->bindValue(":valor_compra", $valor_compra);
@@ -45,9 +48,16 @@ $res->bindValue(":estoque", $estoque);
 
 
 $res2->bindValue(":valor", $valor_total);
-$res2->bindValue(":funcionario", $funcionario);
+$res2->bindValue(":funcionario", $_SESSION['cpf_usuario']);
 
 $res->execute();
+$res2->execute();
+
+$res3->bindValue(":valor", $valor_total);
+$res3->bindValue(":funcionario", $_SESSION['cpf_usuario']);
+$res3->bindValue(":id_conta", $id_conta);
+
+$res3->execute();
 
 echo 'Salvo com Sucesso!';
 
