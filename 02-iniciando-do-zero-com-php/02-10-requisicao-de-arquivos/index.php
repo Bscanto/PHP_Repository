139e0de7df3f53f8ac9ff90eea36eec04b79f1dc
Bruno Canto <bruno_scanto@hotmail.com>
2025-02-28 14:06:11 -0300

    FSPHP Aula 02

---
+++ 02-iniciando-do-zero-com-php/02-10-requisicao-de-arquivos/index.php
@@ -0,0 +1,42 @@
+<?php
+require __DIR__ . '/../../fullstackphp/fsphp.php';
+fullStackPHPClassName("02.10 - Requisição de arquivos");
+
+/*
+ * [ include ] https://php.net/manual/pt_BR/function.include.php
+ * [ include_once ] https://php.net/manual/pt_BR/function.include-once.php
+ */
+fullStackPHPClassSession("include, include_once", __LINE__);
+
+//include "file.php";
+//echo "<p>Continue</p>";
+
+//include "header.php";
+include __DIR__ . "/header.php";
+
+$profile = new StdClass();
+$profile->name = "Robson";
+$profile->company = "UpInside";
+$profile->email = "cursos@upinside.com.br";
+include __DIR__ . "/profile.php";
+
+$profile = new StdClass();
+$profile->name = "Kaue";
+$profile->company = "UpInside";
+$profile->email = "cursos@upinside.com.br";
+include __DIR__ . "/profile.php";
+
+/*
+ * [ require ] https://php.net/manual/pt_BR/function.require.php
+ * [ require_once ] https://php.net/manual/pt_BR/function.require-once.php
+ */
+fullStackPHPClassSession("require, require_once", __LINE__);
+
+//require "file.php";
+//echo "<p>Continue</p>";
+
+require __DIR__ . "/config.php";
+require_once __DIR__ . "/config.php";
+
+echo "<h1>" . COURSE . "</h1>";
+
---
+++ 02-iniciando-do-zero-com-php/02-11-trabalhando-com-funcoes/index.php
@@ -0,0 +1,49 @@
+<?php
+require __DIR__ . '/../../fullstackphp/fsphp.php';
+fullStackPHPClassName("02.11 - Trabalhando com funções");
+
+/*
+ * [ functions ] https://php.net/manual/pt_BR/language.functions.php
+ */
+fullStackPHPClassSession("functions", __LINE__);
+
+require __DIR__ . "/functions.php";
+
+var_dump(functionName("Pearl Jam", "AC\DC", "Alter Bridge"));
+var_dump(functionName("Robson", "Kaue", "Gustavo"));
+
+
+var_dump(optionArgs("Robson"));
+var_dump(optionArgs("Robson", "Kaue"));
+var_dump(optionArgs("Robson", "Kaue", "Gustavo"));
+
+
+/*
+ * [ global access ] global $var
+ */
+fullStackPHPClassSession("global access", __LINE__);
+
+$weight = 86;
+$height = 1.83;
+echo calcImc();
+
+
+/*
+ * [ static arguments ] static $var
+ */
+fullStackPHPClassSession("static arguments", __LINE__);
+
+$pay = payTotal(200);
+$pay = payTotal(150);
+$pay = payTotal(500);
+
+echo $pay;
+
+
+/*
+ * [ dinamic arguments ] get_args | num_args
+ */
+fullStackPHPClassSession("dinamic arguments", __LINE__);
+
+
+var_dump(myTeam("Kaue", "Gustavo", "Gah", "João"));
